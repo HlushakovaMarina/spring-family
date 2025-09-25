76ac6cece785234f587ab0b7808720e9bd3775e5
hlushakovamarina <truhina_m@mail.ru>
2025-09-25 15:55:28 +0300

    домашка про семью доработка

--- src/main/java/hlushakovaM/FamilyApplication.java
+++ src/main/java/hlushakovaM/FamilyApplication.java
@@ -1,7 +1,9 @@
 package hlushakovaM;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FamilyApplication {
 
     public static void main(String[] args) {
